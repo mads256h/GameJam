0d64bb7746d3f3afbe285f6dc05c88ef51b5e917
julio-hb <julius@juto.dk>
2017-05-20 17:26:29 +0200

    Startscreen

---
+++ Assets/Scripts/Startscreen_Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Startscreen_Camera : MonoBehaviour {
+
+    public GameObject fade;
+
+	public void LoadGame()
+    {
+        SceneManager.LoadSceneAsync("main");
+    }
+
+    public void StartFade()
+    {
+        fade.SetActive(true);
+    }
+}
