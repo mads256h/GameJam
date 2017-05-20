6b0b86e4d5d0cd1b678a1094b26c8dd395e49cf9
Gaden <gade1201@gmail.com>
2017-05-20 14:11:43 +0200

    +Playermanager

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public int PlayerID;
+
+    public float Health;
+    public float Score;
+
+    public float lerpSpeed;
+    public Image HealthBar;
+
+    void Update()
+    {
+        HealthBar.fillAmount = Mathf.Lerp(HealthBar.fillAmount, Health / 100, Time.deltaTime * lerpSpeed);
+    }
+
+    public void AddScore (float scoreToAdd)
+    {
+        Score = Score + scoreToAdd;
+    } 
+
+    public void TakeDamage (float damageToLose) {
+        Health = Health - damageToLose;
+	}
+
+    public void GainHeath (float healthToGain)
+    {
+        Health = Health + healthToGain;
+    }
+}
