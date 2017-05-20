f427011b5adac7db399478a856dac66daa9d1b49
julio-hb <julius@juto.dk>
2017-05-20 12:07:36 +0200

    Movement
    sæt speed til 5.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    //speed = 5 er en passende hastighed.
+    public float speed = 5;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        transform.Translate(input * speed * Time.deltaTime);
+	}
+}
