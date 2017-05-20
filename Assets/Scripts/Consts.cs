a4aace968d9bcb2ed0b138c297b268fcc23b1ccf
Mads Mogensen <lort256c@gmail.com>
2017-05-20 14:38:57 +0200

    Added consts and rotations

---
+++ Assets/Scripts/Consts.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consts : MonoBehaviour
+{
+
+    public enum PlayerID
+    {
+        One = 1,
+        Two = 2,
+    }
+}
---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour {
+
+    
+    public Consts.PlayerID PlayerID = Consts.PlayerID.One;
+    private new Transform transform;
+
+
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float angle = 0;
+
+        switch (PlayerID)
+        {
+            case Consts.PlayerID.One:
+                angle = Mathf.Atan2(Input.GetAxis("RotVertical1"), Input.GetAxis("RotHorizontal1")) * Mathf.Rad2Deg;
+                break;
+
+            case Consts.PlayerID.Two:
+                angle = Mathf.Atan2(Input.GetAxis("RotVertical2"), Input.GetAxis("RotHorizontal2")) * Mathf.Rad2Deg;
+                break;
+        }
+        if (angle == 0f) //If angle is zero there is no need to update
+            return;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, (angle + 90f) * -1);
+
+	}
+}
