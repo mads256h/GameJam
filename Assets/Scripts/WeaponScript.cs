798f29ff4fa0a3470f3e05e8a0abe82c9b47b8a9
Mads Mogensen <lort256c@gmail.com>
2017-05-20 16:39:53 +0200

    Fix

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class WeaponScript : MonoBehaviour {
 
-    public GameObject Bullet;
-    public float FireRate = 1f;
-    private float timer;
-    private int bulletCounter;
+    public Bullet bullet;
 
-<<<<<<< HEAD
-    void Start ()
-=======
     public Transform Player;
 
     public float Cooldown = 0.5f;
@@ -19,31 +13,18 @@ public class WeaponScript : MonoBehaviour {
     private float timer = 0f;
 
     void Start()
->>>>>>> Bullet
     {
         Player = GetComponent<Transform>();
     }
 
     void Update ()
     {
-<<<<<<< HEAD
-        //If Fire button is pressed and timer is > fire rate -- Shoot
-        timer += Time.deltaTime;
-        if (Input.GetButton("Fire1") && timer > FireRate)
-        {
-            GameObject Projectile = Instantiate(Bullet, transform);
-            Projectile.name = "Projectile " + bulletCounter;
-            bulletCounter++;
-            timer = 0;
-=======
-        timer += Time.deltaTime;
         if (Input.GetButton("Fire1") && timer >= Cooldown)
         {
             Vector3 rotation = Player.rotation.eulerAngles;
             rotation.y -= 90f;
-            Instantiate(Bullet, Player.position, Quaternion.Euler(rotation));
+            Instantiate(bullet, Player.position, Quaternion.Euler(rotation));
             timer = 0f;
->>>>>>> Bullet
         }
 	}
 }
