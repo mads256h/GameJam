c34095b37b749a99de4dd60547c90a5ad27a6cb0
Zim79 <simon.d.laursen.o.o@gmail.com>
2017-05-20 16:04:02 +0200

    FireSpread added

---
+++ Assets/Scripts/FireSpread.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpread : MonoBehaviour {
+
+    public bool OnFire;
+    void Start ()
+    {
+
+	}
+	
+
+	void Update ()
+    {
+        if (OnFire)
+        {
+            GetComponentInChildren<ParticleSystem>().Play();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.GetComponent<FireSpread>() != null)
+        {
+            collision.gameObject.GetComponent<FireSpread>().OnFire = true;
+        }
+    }
+
+}
+
