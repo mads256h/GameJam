35f86d169f352a192b4709987607d7d2a3e24d51
Mads Mogensen <lort256c@gmail.com>
2017-05-21 04:58:48 +0200

    Added shock ult

---
+++ Assets/Scripts/UltShock.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltShock : MonoBehaviour
+{
+
+    public float ShockRadius = 64f;
+
+    public int Damage = 10;
+
+    public float TimeStunned = 5f;
+
+    public float TimeActive = 0.5f;
+
+    public new GameObject particleSystem;
+
+    public Vector3 ParticleSystemRotation = Vector3.zero;
+
+    private CapsuleCollider capsuleCollider;
+
+    private float timer = 0f;
+
+    private bool toRemove = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        capsuleCollider.radius = ShockRadius;
+        capsuleCollider.enabled = false;
+        Explode();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= TimeActive && toRemove)
+            Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        capsuleCollider.enabled = true;
+        toRemove = true;
+        timer = 0f;
+        ((GameObject)Instantiate(particleSystem, transform.position, Quaternion.Euler(ParticleSystemRotation))).transform.localScale = new Vector3(1 * 32, 1 * 32, 1 * 32);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.GetComponent<Enemy>().StartCoroutine(other.gameObject.GetComponent<Enemy>().TakeDamage(Damage));
+            //other.GetComponent<Enemy>().StartCoroutine(other.gameObject.GetComponent<Enemy>().Stun(TimeStunned));
+        }
+
+        Destroy(gameObject);
+    }
+}
